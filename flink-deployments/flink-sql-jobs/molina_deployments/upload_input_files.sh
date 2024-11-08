6e741781dd19a85af4793a8159f6aa376a90917b
Sasidaren Dinakaran <sdinakaran@psyncopate.com>
2024-11-08 10:18:46 -0500

    deployments for Molina

---
+++ flink-deployments/flink-sql-jobs/molina_deployments/upload_input_files.sh
@@ -0,0 +1,5 @@
+kubectl cp ../inputFiles/claim_diagnosis/claim_diagnosis1.csv ingest-claim-diagnosis-data-taskmanager-1-1:/inputFiles/claim_diagnosis
+
+kubectl cp ../inputFiles/claim_procedure/claim_procedure1.csv ingest-claim-procedure-data-taskmanager-1-1:/inputFiles/claim_procedure
+
+kubectl cp ../inputFiles/claim_providers/claim_providers1.csv ingest-claim-provider-data-taskmanager-1-1:/inputFiles/claim_provider
